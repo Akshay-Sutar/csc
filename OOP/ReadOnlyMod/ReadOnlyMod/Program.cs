7b48d29ef27dc2f64aec2a74cda5d8834124745c
Akshay Sutar <akshay.sutar5@outlook.com>
2021-09-28 17:57:17 +0530

    readonly

---
+++ OOP/ReadOnlyMod/ReadOnlyMod/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReadOnlyMod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadOnlyMod Obj = new ReadOnlyMod(3);
+            Obj.x = 1;
+            Obj.print();
+        }
+    }
+}
---
+++ OOP/ReadOnlyMod/ReadOnlyMod/ReadOnlyMod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ReadOnlyMod
+{
+    class ReadOnlyMod
+    {
+        public int x;
+        public readonly int y = 2;
+        public readonly int z;
+
+        public ReadOnlyMod(int z)
+        {
+            this.z = z;
+        }
+
+        public void print()
+        {
+            Console.WriteLine($" x = {x}");
+            Console.WriteLine($" y = {y}");
+            Console.WriteLine($" z = {z}");
+        }
+    }
+}
