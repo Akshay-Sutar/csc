7a94d94642e235e37216048a1f20ba5cbd388e13
Akshay Sutar <akshay.sutar5@outlook.com>
2021-09-28 17:10:52 +0530

    add null coalesce operator

---
+++ OOP/NullCoalasceOp/NullCoalasceOp/NullCoalesceOp.cs
@@ -0,0 +1,16 @@
+﻿namespace NullCoalasceOp
+{
+    class NullCoalesceOp
+    {
+        int? _a;
+
+        public NullCoalesceOp(int? value)
+        {
+            _a = value ?? 0;
+        }
+        public int getValue()
+        {
+            return _a ?? 0;
+        }
+    }
+}
---
+++ OOP/NullCoalasceOp/NullCoalasceOp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NullCoalasceOp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NullCoalesceOp a = new NullCoalesceOp(5);
+            NullCoalesceOp b = new NullCoalesceOp(null);
+            Console.WriteLine("a value is "+a.getValue());
+            Console.WriteLine("b value is "+b.getValue());
+        }
+    }
+}
