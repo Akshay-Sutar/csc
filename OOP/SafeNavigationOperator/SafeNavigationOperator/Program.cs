a7834bcaa4fa688a4eb6a385c2f6add41130db80
Akshay Sutar <akshay.sutar5@outlook.com>
2021-09-28 17:24:13 +0530

    added safe evaluation operator

---
+++ OOP/SafeNavigationOperator/SafeNavigationOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SafeNavigationOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SafeNavigationOperator person = new SafeNavigationOperator("AAS",null,"ABC");
+            Console.WriteLine("First Name is :"+person?.Fname);
+            Console.WriteLine("Second Name is :"+person?.Mname);
+            Console.WriteLine("Last Name is :"+person?.Lname);
+
+            SafeNavigationOperator person2 = null;
+            Console.WriteLine("First Name is :" + person2?.Fname);
+            Console.WriteLine("Second Name is :" + person2?.Mname);
+            Console.WriteLine("Last Name is :" + person2?.Lname);
+        }
+    }
+}
---
+++ OOP/SafeNavigationOperator/SafeNavigationOperator/SafeNavigationOperator.cs
@@ -0,0 +1,13 @@
+﻿namespace SafeNavigationOperator
+{
+    class SafeNavigationOperator
+    {
+        public string Fname, Mname, Lname;
+        public SafeNavigationOperator(string fname, string? mname, string lname)
+        {
+            Fname = fname;
+            Mname = mname;
+            Lname = lname;
+        }
+    }
+}
