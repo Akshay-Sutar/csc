2e97efef82d0ed8dea21fa67d9a0449a9c8bd9e9
Akshay Sutar <akshay.sutar5@outlook.com>
2021-10-02 19:09:38 +0530

    added const

---
+++ OOP/ConstantKeyword/ConstantKeyword/ConstantKeyword.cs
@@ -0,0 +1,17 @@
+﻿namespace ConstantKeyword
+{
+    class ConstantKeyword
+    {
+        private const float _pi = 3.1419f;
+
+        public float getArea(float radius)
+        {
+            return _pi * radius * radius;
+        }
+
+        public float getVolume(float radius, float height)
+        {
+            return getArea(radius) * height;
+        }
+    }
+}
---
+++ OOP/ConstantKeyword/ConstantKeyword/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConstantKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConstantKeyword shape = new ConstantKeyword();
+            float radius = 10f;
+            float height = 12f;
+
+            Console.WriteLine("Area is : " + shape.getArea(radius));
+            Console.WriteLine("Volume is : " + shape.getVolume(radius, height));
+        }
+    }
+}
