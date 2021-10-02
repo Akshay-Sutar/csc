d2ae7de56006146d5064364a172b7de9ef010e1e
Akshay Sutar <akshay.sutar5@outlook.com>
2021-10-02 19:12:23 +0530

    added comment

--- OOP/ReadOnlyMod/ReadOnlyMod/ReadOnlyMod.cs
+++ OOP/ReadOnlyMod/ReadOnlyMod/ReadOnlyMod.cs
@@ -3,6 +3,7 @@ namespace ReadOnlyMod
 {
     class ReadOnlyMod
     {
+        // read only is run time constants that can be initialized at declaration or in constructor and so can have different values based on constructor.
         public int x;
         public readonly int y = 2;
         public readonly int z;
