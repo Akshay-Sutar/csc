6a138200f8d6137bd69c186daec15890cdc8e8c1
Akshay Sutar <akshay.sutar5@outlook.com>
2021-10-02 19:10:56 +0530

    added comment

--- OOP/ConstantKeyword/ConstantKeyword/ConstantKeyword.cs
+++ OOP/ConstantKeyword/ConstantKeyword/ConstantKeyword.cs
@@ -2,6 +2,7 @@
 {
     class ConstantKeyword
     {
+        //compile time constants that needs to be defined during declaration
         private const float _pi = 3.1419f;
 
         public float getArea(float radius)
