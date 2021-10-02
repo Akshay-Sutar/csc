1cd1fc364d75d3bdad96a0b68757dae9fe241365
Akshay Sutar <akshay.sutar5@outlook.com>
2021-10-02 19:41:26 +0530

    added references

---
+++ OOP/References/References/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace References
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            References obj = new References(100);
+            obj.printNumber();
+            References.changeReference(ref obj);
+            obj.printNumber();
+        }
+    }
+}
---
+++ OOP/References/References/References.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace References
+{
+    class References
+    {
+        private int _number;
+
+        public References(int number)
+        {
+            _number = number;
+        }
+
+        public void printNumber()
+        {
+            Console.WriteLine($"No is {_number}");
+        }
+
+        public static void changeReference(ref References obj) {
+            obj = new References(10); 
+        }
+    }
+}
