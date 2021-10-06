24e67b0ec45b18a3c63ea38c65ef1d441f6581c5
Akshay Sutar <akshay.sutar5@outlook.com>
2021-10-06 18:48:17 +0530

    added param keyword

---
+++ OOP/ParamKeyword/ParamKeyword/ParamKeyword.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ParamKeyword
+{
+    class ParamKeyword
+    {
+        public void printList(params object[] list)
+        {
+            for (int i = 0; i < list.Length; ++i)
+            {
+                Console.WriteLine($"item at {i} is {list[i]}");
+            }
+        }
+    }
+}
---
+++ OOP/ParamKeyword/ParamKeyword/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ParamKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParamKeyword obj = new ParamKeyword();
+            obj.printList(1, 2, 3, 4, 5);
+            obj.printList("this","is","a","test");
+        }
+    }
+}
