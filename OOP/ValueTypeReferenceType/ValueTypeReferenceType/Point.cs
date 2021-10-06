f2fa4440106c62b7e463b4dfb51fdffe675dc3d9
Akshay Sutar <akshay.sutar5@outlook.com>
2021-10-06 19:05:16 +0530

    added value type and reference type

---
+++ OOP/ValueTypeReferenceType/ValueTypeReferenceType/Point.cs
@@ -0,0 +1,25 @@
+﻿namespace ValueTypeReferenceType
+{
+    class Point
+    {
+        public int X, Y;
+
+        public Point(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public void swap()
+        {
+            int z = X;
+            X = Y;
+            Y = z;
+        }
+
+        public override string ToString()
+        {
+            return $"X: {X}, Y: {Y}";
+        }
+    }
+}
---
+++ OOP/ValueTypeReferenceType/ValueTypeReferenceType/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ValueTypeReferenceType
+{
+    /*
+        Value type is a variable contains an instance.
+        Reference type contains a reference of an instance.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1,p2;
+            p1 = new Point(1,2);
+            Console.WriteLine(p1.ToString());
+            p1.swap();
+            Console.WriteLine(p1.ToString());
+            p2 = p1;
+            p2.X = 100;
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(p2.ToString());
+        }
+    }
+}
