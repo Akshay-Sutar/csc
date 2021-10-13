9aa64be73366c1b9a1f7cec7c39f99be4eceb7e7
Akshay Sutar <akshay.sutar5@outlook.com>
2021-10-13 18:42:36 +0530

    added is op and boxing

---
+++ OOP/Boxing/Boxing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            // CLR is boxing the int i to Object o
+            Object o = i;
+
+            Console.WriteLine($" i value is {i}");
+            Console.WriteLine($" o value is {o}");
+               
+            // after changing o value, i is not affected
+            o = 20;
+            Console.WriteLine($" i value is {i}");
+            Console.WriteLine($" o value is {o}");
+
+            // o is unboxed
+            int j = (int)o;
+            Console.WriteLine($" i value is {i}");
+            Console.WriteLine($" i value is {i}");
+            Console.WriteLine($" j value is {j}");
+        }
+    }
+}
---
+++ OOP/IsOperator/IsOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IsOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int? y = null;
+
+            if (x is int)
+            {
+                Console.WriteLine("x is an integer");
+            }
+
+            if (y is null)
+            {
+                Console.WriteLine("y is null");
+            }
+        }
+    }
+}
