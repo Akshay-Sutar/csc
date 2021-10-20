68afb18857ffefe7f6e411bca212a4502815bf98
Akshay Sutar <akshay.sutar5@outlook.com>
2021-10-20 19:18:24 +0530

    added dynamic objects

---
+++ OOP/DynamicObject/DynamicObject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Dynamic;
+
+namespace DynamicObject
+{
+    class Program
+    {
+        /*
+            create dynamic members
+         */
+        static void Main(string[] args)
+        {
+            dynamic Employee = new ExpandoObject();
+
+            Employee.name = "James Bond";
+            Employee.age = 35;
+
+            dynamic Employer = new ExpandoObject();
+            Employer.name = "MI6";
+            Employer.age = 105;
+
+            printObj(Employee);
+            printObj(Employer);
+        }
+
+        public static void printObj(dynamic obj)
+        {
+            Console.WriteLine($"name is {obj.name} and age is {obj.age}");
+        }
+    }
+}
