eaf49f5b27da1f1675391f283d813bcb1b4a4a6d
Akshay Sutar <akshay.sutar5@outlook.com>
2021-11-01 19:04:42 +0530

    added delegate

---
+++ OOP/Delegates/Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Delegates
+{
+    class Program
+    {
+        public delegate void printString(string s);
+
+        public static void printScreen(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        public static void printFile(string s)
+        {
+            FileStream fs = new FileStream("./temp.txt", FileMode.Append);
+            StreamWriter sr = new StreamWriter(fs);
+            sr.Write(s);
+            sr.Flush();
+            sr.Close();
+            fs.Close();
+        }
+        static void Main(string[] args)
+        {
+            printString ps1 = new printString(printScreen);
+            printString ps2 = new printString(printFile);
+
+            ps1("This is on screen");
+            ps2("This is in a file");
+        }
+    }
+}
